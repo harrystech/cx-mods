b8e395b101e3f1405e71e54de9a7633baca84563
Cassie Malatak <cjmalatak@gmail.com>
2018-12-17 13:13:42 -0500

    Update inject.js
    Save reference to header tabs

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,33 @@
+
+const injectSearchByAddress = () => {
+	headerTabs = document.querySelector('#tabs');
+	console.log('Header Tabs', headerTabs);
+}
+
+const initialize = () => {
+	injectSearchByAddress();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
+		initialize();
 	}
 	}, 10);
 });
