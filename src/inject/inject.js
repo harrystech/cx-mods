5330b57f18f96f94c35f2acaf6d5d930486d8bf6
Babbins <thealimahmoud@gmail.com>
2018-12-17 13:42:04 -0500

    Added search by address functionality

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,33 +1,33 @@
-
 const injectSearchByAddress = () => {
-	headerTabs = document.querySelector('#tabs');
-	console.log('Header Tabs', headerTabs);
-}
+  headerTabs = document.querySelector("#tabs");
+  if (!headerTabs) {
+    return;
+  }
+  const searchByAddressListItem = document.createElement("li");
+  searchByAddressListItem.setAttribute("class", "cx-mods");
+  searchByAddressListItem.setAttribute("id", "search-by-address");
+  const searchByAddressHTML = `
+    <form action="/admin/shipping_addresses">
+			<input name="q[name_cont]" type="text" placeholder="Name (contains)">
+			<input name="q[address_line_1_cont]" type="text" placeholder="Address (contains)">
+			<input name="q[zip_eq]" type="text" placeholder="Zip (exact)">
+			<input type="submit" class="cx-mods-hidden">
+    </form>
+	`;
+
+  searchByAddressListItem.innerHTML = searchByAddressHTML;
+  headerTabs.appendChild(searchByAddressListItem);
+};
 
 const initialize = () => {
-	injectSearchByAddress();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  injectSearchByAddress();
+};
 
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-		initialize();
-	}
-	}, 10);
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
+      initialize();
+    }
+  }, 10);
 });
