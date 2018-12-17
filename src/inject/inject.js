d937a44f64b926def8409d763895067b482dd5c5
Cassie Malatak <cjmalatak@gmail.com>
2018-12-17 17:55:42 -0500

    Suggests 17 days out for shave_plan_first_send_date date picker

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -8,7 +8,7 @@ const injectSearchByAddress = () => {
   searchByAddressListItem.setAttribute("id", "search-by-address");
   const searchByAddressHTML = `
 	  <div class="search-by-address-heading">
-  		<h4>Address Search</h4>
+	  	<h4>Address Search</h4>
 	  </div>
     <form action="/admin/shipping_addresses" id="shipping-address-form">
 			<input name="q[name_cont]" type="text" placeholder="Name (contains)">
@@ -22,8 +22,37 @@ const injectSearchByAddress = () => {
   headerTabs.appendChild(searchByAddressListItem);
 };
 
+
+const injectSeventeenDays = () => {
+	if (!location.href.includes('/create_order')) return;
+	const datePickerEl = document.querySelector('#shave_plan_first_send_date');
+	if (!datePickerEl) return;
+	const d = new Date();
+	const ts = d.getTime();
+	const seventeenDays = ts + (17 * 24 * 60 * 60 * 1000);
+	const sev = new Date(seventeenDays);
+	console.log(sev.toString());
+	const dateListItemParent = datePickerEl.parentNode
+	
+	const datePickerListItem = document.createElement("li");
+	datePickerListItem.setAttribute("class", "cx-mods");
+	const dateListHTML = `
+		<div class='date-picker-seventeen'>
+			<p>17 days out would be:</p>
+			<p>${sev.toString()};</p>
+		</div>
+	`;
+	
+	datePickerListItem.innerHTML = dateListHTML;
+	dateListItemParent.appendChild(datePickerListItem);
+
+}
+
+
+
 const initialize = () => {
   injectSearchByAddress();
+  injectSeventeenDays();
 };
 
 chrome.extension.sendMessage({}, function(response) {
