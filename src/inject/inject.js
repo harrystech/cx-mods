530c3f490fa7883732440837dcc1c7a78113c4b3
Cassie Malatak <cjmalatak@gmail.com>
2018-12-17 15:53:06 -0500

    Add search by address heading; inputs inline;

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,7 +7,10 @@ const injectSearchByAddress = () => {
   searchByAddressListItem.setAttribute("class", "cx-mods");
   searchByAddressListItem.setAttribute("id", "search-by-address");
   const searchByAddressHTML = `
-    <form action="/admin/shipping_addresses">
+	  <div class="search-by-address-heading">
+  		<h4>Address Search</h4>
+	  </div>
+    <form action="/admin/shipping_addresses" id="shipping-address-form">
 			<input name="q[name_cont]" type="text" placeholder="Name (contains)">
 			<input name="q[address_line_1_cont]" type="text" placeholder="Address (contains)">
 			<input name="q[zip_eq]" type="text" placeholder="Zip (exact)">
